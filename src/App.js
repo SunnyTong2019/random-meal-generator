3f7554466edc0453882f398e9f2ed9a2333a20c8
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-02-21 11:27:11 -0800

    handle case when no tags, and only display meal with button click

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import axios from "axios";
 
 
-
 class App extends Component {
   state = {
     meal: {},
@@ -14,27 +13,24 @@ class App extends Component {
   handleGetMeal = () => {
     axios.get("https://themealdb.com/api/json/v1/1/random.php")
       .then(res => {
-        var videoStr = res.data.meals[0].strYoutube;
-        var index = videoStr.indexOf("v=");
-        var videoID = videoStr.slice(index + 2);
-        var mealIngreArr = [];
+        let videoStr = res.data.meals[0].strYoutube;
+        let index = videoStr.indexOf("v=");
+        let videoID = videoStr.slice(index + 2);
+
+        let mealIngreArr = [];
 
         for (let i = 1; i <= 20; i++) {
-          var strIngre = "strIngredient" + i;
-          var strMeasure = "strMeasure" + i;
+          let strIngre = "strIngredient" + i;
+          let strMeasure = "strMeasure" + i;
           if (res.data.meals[0][strIngre]) {
-
             // if (res.data.meals[0][strMeasure]) { mealIngreArr.push(res.data.meals[0][strIngre] + " - " + res.data.meals[0][strMeasure]); }
             // else {
             //   mealIngreArr.push(res.data.meals[0][strIngre])
             // }
-
-           res.data.meals[0][strMeasure] ? mealIngreArr.push(res.data.meals[0][strIngre] + " - " + res.data.meals[0][strMeasure])
-           : mealIngreArr.push(res.data.meals[0][strIngre])
+            res.data.meals[0][strMeasure] ? mealIngreArr.push(res.data.meals[0][strIngre] + " - " + res.data.meals[0][strMeasure])
+              : mealIngreArr.push(res.data.meals[0][strIngre])
           }
         }
-
-        console.log(res.data.meals[0]);
 
         this.setState({
           meal: res.data.meals[0],
@@ -54,37 +50,33 @@ class App extends Component {
 
       <div className="card-body">
         <h5 className="card-title">Don't know what to eat?</h5>
-        <p className="card-text">Click button below to generate a random meal with cooking instructions</p>
+        <p className="card-text">Click button below to generate a random meal with cooking instructions.</p>
         <button className="btn btn-primary" onClick={this.handleGetMeal}>Get Meal</button>
       </div>
 
-
-
-      <div className="card-footer">
-
-        <div className="card mb-3">
-          <div className="card-body">
-            <h4 className="card-title">{this.state.meal.strMeal}</h4>
-            <img width="560" height="315" src={this.state.meal.strMealThumb} alt="meal" className="mb-4" />
-            <p><strong>Category: </strong><span id="category">{this.state.meal.strCategory}</span></p>
-            <p><strong>Area: </strong><span id="area">{this.state.meal.strArea}</span></p>
-            <p><strong>Tags: </strong><span id="tags">{this.state.meal.strTags}</span></p>
-            <p><strong>Ingredients:</strong></p>
-            <ul id="ingredients">
-              { this.state.mealIngredients.map ( (ingre,index) => <li key={index+1}>{ingre}</li> ) }
-            </ul>
-            <p><strong>Cooking Instructions:</strong></p>
-            <p id="instructions">{this.state.meal.strInstructions}</p>
-            <p><strong>Video Recipe:</strong></p>
-            <iframe width="560" height="315" title="video receipt" src={"http://www.youtube.com/embed/" + this.state.mealVideo}
-              frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+      {Object.keys(this.state.meal).length !== 0 ? (
+        <div className="card-footer">
+          <div className="card mb-3">
+            <div className="card-body">
+              <h4 className="card-title">{this.state.meal.strMeal}</h4>
+              <img width="560" height="315" src={this.state.meal.strMealThumb} alt="meal" className="mb-4" />
+              <p><strong>Category: </strong><span id="category">{this.state.meal.strCategory}</span></p>
+              <p><strong>Area: </strong><span id="area">{this.state.meal.strArea}</span></p>
+              {this.state.meal.strTags ? <p><strong>Tags: </strong><span id="tags">{this.state.meal.strTags}</span></p> : null}
+              <p><strong>Ingredients:</strong></p>
+              <ul id="ingredients">
+                {this.state.mealIngredients.map((ingre, index) => <li key={index + 1}>{ingre}</li>)}
+              </ul>
+              <p><strong>Cooking Instructions:</strong></p>
+              <p id="instructions">{this.state.meal.strInstructions}</p>
+              <p><strong>Video Recipe:</strong></p>
+              <iframe width="560" height="315" title="video receipt" src={"http://www.youtube.com/embed/" + this.state.mealVideo}
+                frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            </div>
           </div>
-        </div>
-
-      </div>
-
-
-
+        </div>)
+        : null
+      }
 
     </div>
     );

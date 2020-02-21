85883beb739a28728e0adb9d2a0c4a6a69974715
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2020-02-21 11:56:45 -0800

    Updates

---
+++ precache-manifest.384d3a333ebdffc3077310032d18ab31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ff9a09a0ff9d284f8e87990a9550cdf",
+    "url": "/random-meal-generator/index.html"
+  },
+  {
+    "revision": "e195e75db925451a9a9d",
+    "url": "/random-meal-generator/static/css/main.64709b38.chunk.css"
+  },
+  {
+    "revision": "70011a15c5caac4f1632",
+    "url": "/random-meal-generator/static/js/2.5e4c228e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/random-meal-generator/static/js/2.5e4c228e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e195e75db925451a9a9d",
+    "url": "/random-meal-generator/static/js/main.982b3e63.chunk.js"
+  },
+  {
+    "revision": "b679758affbfdb1605bb",
+    "url": "/random-meal-generator/static/js/runtime-main.210e42a6.js"
+  }
+]);
